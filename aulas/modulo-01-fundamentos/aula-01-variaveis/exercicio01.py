36176e3c6fd842120508519a6141ef4d5964eb36
Gabriel Estanislau <ggstanis@gmail.com>
2026-07-11 17:36:43 -0300

    feat: adiciona exercícios sobre variáveis e entrada de dados em Python

---
+++ aulas/modulo-01-fundamentos/aula-01-variaveis/exercicio01.py
@@ -0,0 +1,15 @@
+"""
+Módulo 1 - Fundamentos
+Aula 1 - Variáveis
+Exercício 01
+Autor: Gabriel Estanislau
+Data: 11/07/2026
+"""
+
+nome = "Gabriel"
+idade = 32
+cidade = "Belo Horizonte"
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Cidade:", cidade)
---
+++ aulas/modulo-01-fundamentos/aula-01-variaveis/exercicio02.py
@@ -0,0 +1,15 @@
+"""
+Módulo 1 - Fundamentos Python
+Aula 1.2 - Entrada de Dados
+Exercício 02
+Autor: Gabriel Estanislau
+Data: 11/07/2026
+"""
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+cidade = input("Digite sua cidade: ")
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Cidade:", cidade)
