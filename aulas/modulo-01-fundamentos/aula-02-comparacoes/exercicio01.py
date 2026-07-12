1a7f1afc1a22cb35bef6ee0bb29360a657f475bb
Gabriel Estanislau <ggstanis@gmail.com>
2026-07-12 15:59:21 -0300

    feat: adiciona exercícios sobre comparações em Python

---
+++ aulas/modulo-01-fundamentos/aula-02-comparacoes/exercicio01.py
@@ -0,0 +1,19 @@
+"""
+Módulo 1 - Fundamentos
+Aula 2 - Comparações
+Exercício 01
+Autor: Gabriel Estanislau
+Data: 12/07/2026
+"""
+
+idade = 18
+maior = idade > 18
+print(maior)
+
+nome = "Gabriel"
+resultado = nome == "Gabriel"
+print(resultado)
+
+idade = 32
+print(idade == "32")
+print(idade != "32")
