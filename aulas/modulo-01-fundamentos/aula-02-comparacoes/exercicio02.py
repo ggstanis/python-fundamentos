285ae9112d866394159c295d57d675bf6f02462e
Gabriel Estanislau <ggstanis@gmail.com>
2026-07-12 16:44:52 -0300

    feat: adiciona exercicio de comparações lógicas

---
+++ aulas/modulo-01-fundamentos/aula-02-comparacoes/exercicio02.py
@@ -0,0 +1,19 @@
+"""
+Módulo 1 - Fundamentos
+Aula 2.1 - Operadores Lógicos
+Exercício 02
+Autor: Gabriel Estanislau
+Data: 12/07/2026
+"""
+
+idade = 18
+maior = idade >= 18 and idade <= 30
+print(maior)
+
+nome = "Gabriel"
+resultado = nome == "Gabriel Estanislau" or nome == "Gabriel"
+print(resultado)
+
+menor_de_idade = not idade
+
+print(menor_de_idade)
